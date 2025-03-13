ed5fc126cffe79df5e7bf2b127857db65f6c2ce1
Jazzy DelaCruz <micodelacruz519@gmai.com>
2025-03-13 12:28:27 +0800

    set diet

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,4 +1,4 @@
-3import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
